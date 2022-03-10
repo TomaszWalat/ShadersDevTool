8ae128a29d7904f0b43ab6464ba43d0d896abc3e
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2022-03-10 13:36:15 +0000

    Implemented basic Diffuse shading
    Added model folder
    
    Renamed basic_uniform.vert -> basicShader.vert
    Renamed basic_uniform.frag -> basicShader.frag
    
    3rd Party files:
    Added drawable.h
    Added trianglemesh.h (and .cpp)
    Added torus.h (and .cpp)
    Added teapot.h (and .cpp)
    Added teapotdata.h

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -2,87 +2,65 @@
 
 #include <cstdio>
 #include <cstdlib>
-
-#include <string>
-using std::string;
-
 #include <iostream>
-using std::cerr;
-using std::endl;
+#include <string>
+
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "helper/glutils.h"
 
 using glm::vec3;
+using glm::vec4;
+using glm::mat3;
+using glm::mat4;
 
-SceneBasic_Uniform::SceneBasic_Uniform() : angle(0.0f) {}
+SceneBasic_Uniform::SceneBasic_Uniform() : torus(0.7f, 0.3f, 30, 30) {}
 
 void SceneBasic_Uniform::initScene()
 {
     compile();
 
-    std::cout << std::endl;
+	glEnable(GL_DEPTH_TEST);
 
-    prog.printActiveUniforms();
+	model = mat4(1.0f);
+	model[3][1] = -2.5f;
 
-    /////////////////// Create the VBO ////////////////////
-    float positionData[] = {
-        -0.8f, -0.8f, 0.0f,
-         0.8f, -0.8f, 0.0f,
-         0.0f,  0.8f, 0.0f };
-    float colorData[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f };
+	view = lookAt(vec3(0.0f, 0.0f, 5.0f), vec3(0.0f, -2.5f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 
-    // Create and populate the buffer objects
-    GLuint vboHandles[2];
-    glGenBuffers(2, vboHandles);
-    GLuint positionBufferHandle = vboHandles[0];
-    GLuint colorBufferHandle = vboHandles[1];
+	projection = mat4(1.0f);
 
-    glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), positionData, GL_STATIC_DRAW);
+	prog.setUniform("Ka", vec3(0.0f, 0.0f, 0.5f));
+	prog.setUniform("Kd", vec3(0.5f, 0.5f, 1.0f));
+	prog.setUniform("Ks", vec3(1.0f, 1.0f, 1.0f));
 
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), colorData, GL_STATIC_DRAW);
-
-    // Create and set-up the vertex array object
-    glGenVertexArrays( 1, &vaoHandle );
-    glBindVertexArray(vaoHandle);
-
-    glEnableVertexAttribArray(0);  // Vertex position
-    glEnableVertexAttribArray(1);  // Vertex color
-
-    #ifdef __APPLE__
-        glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
-        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
-
-        glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
-    #else
-    		glBindVertexBuffer(0, positionBufferHandle, 0, sizeof(GLfloat)*3);
-    		glBindVertexBuffer(1, colorBufferHandle, 0, sizeof(GLfloat)*3);
-
-    		glVertexAttribFormat(0, 3, GL_FLOAT, GL_FALSE, 0);
-    		glVertexAttribBinding(0, 0);
-    		glVertexAttribFormat(1, 3, GL_FLOAT, GL_FALSE, 0);
-    	  glVertexAttribBinding(1, 1);
-    #endif
-    glBindVertexArray(0);
+	prog.setUniform("La", vec3(0.1f, 0.1f, 0.1f));
+	prog.setUniform("Ld", vec3(0.5f, 0.5f, 0.5f));
+	prog.setUniform("Ls", vec3(1.0f, 1.0f, 1.0f));
+	prog.setUniform("LightPosition", model * view * vec4(5.0f, 5.0f, 0.0f, 1.0f));
 }
 
 void SceneBasic_Uniform::compile()
 {
 	try {
-		prog.compileShader("shader/basic_uniform.vert");
-		prog.compileShader("shader/basic_uniform.frag");
+		prog.compileShader("shader/basicShader.vert");
+		prog.compileShader("shader/basicShader.frag");
 		prog.link();
 		prog.use();
 	} catch (GLSLProgramException &e) {
-		cerr << e.what() << endl;
+		std::cerr << e.what() << std::endl;
 		exit(EXIT_FAILURE);
 	}
 }
+
+void SceneBasic_Uniform::setMatrices()
+{
+	mat4 mv = view * model;
+
+	prog.setUniform("ModelViewMatrix", mv);
+	prog.setUniform("NormalMatrix", mat3(mv[0], mv[1], mv[2]));
+	prog.setUniform("MVP", projection * mv);
+}
+
 
 void SceneBasic_Uniform::update( float t )
 {
@@ -91,19 +69,19 @@ void SceneBasic_Uniform::update( float t )
 
 void SceneBasic_Uniform::render()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    //create the rotation matrix here and update the uniform in the shader 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBindVertexArray(vaoHandle);
-    glDrawArrays(GL_TRIANGLES, 0, 3 );
+	setMatrices();
 
-    glBindVertexArray(0);
+	torus.render();
 }
 
 void SceneBasic_Uniform::resize(int w, int h)
 {
     width = w;
     height = h;
+
     glViewport(0,0,w,h);
+
+	projection = glm::perspective(glm::radians(70.0f), (float)w/h, 0.3f, 100.0f);
 }
--- scenebasic_uniform.h
+++ scenebasic_uniform.h
@@ -1,19 +1,22 @@
 #ifndef SCENEBASIC_UNIFORM_H
 #define SCENEBASIC_UNIFORM_H
 
-#include "helper/scene.h"
-
 #include <glad/glad.h>
+#include "glm/glm.hpp"
+
+#include "helper/scene.h"
 #include "helper/glslprogram.h"
+
+#include "model/torus.h"
 
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLuint vaoHandle;
     GLSLProgram prog;
-    float angle;
+    Torus torus;
 
     void compile();
+    void setMatrices();
 
 public:
     SceneBasic_Uniform();
